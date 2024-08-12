198a94dfeec54ddb3584c7328388c268cf060b91
Abasoff <anar.abasov24@gmail.com>
2024-08-12 10:09:45 +0300

    First Commit Upload

---
+++ Assets/Scripts/AmbientEffect.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientEffect : MonoBehaviour
+{
+    private static AmbientEffect instance;
+
+	private void Awake()
+	{
+		if(instance == null){
+			instance = this;
+			DontDestroyOnLoad(instance);
+		} else{ 
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnBuildLeft.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBuildLeft : MonoBehaviour
+{
+    public GameObject[] buildings;
+
+    private void Start()
+    {
+        // Define the allowed angles
+        float[] allowedAngles = { -90 };
+
+        // Select a random building from the array
+        GameObject building = buildings[Random.Range(0, buildings.Length)];
+
+        // Select a random angle from the allowed angles
+        float randomAngle = allowedAngles[Random.Range(0, allowedAngles.Length)];
+
+        // Define the rotation based on the selected angle
+        Quaternion rotation = Quaternion.Euler(0, randomAngle, 0);
+
+        // Instantiate the building with the chosen rotation
+        Instantiate(building, transform.position, rotation);
+    }
+}
